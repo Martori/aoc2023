820f164454e801ec284fd083e0fdd1bb558e3c65
Eric Martori <eric.martori@nestle.com>
2023-12-01 14:02:02 +0100

    solve day one

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,22 @@
+val textDigits = mapOf("one" to "1", "two" to "2", "three" to "3", "four" to "4", "five" to "5", "six" to "6", "seven" to "7", "eight" to "8", "nine" to "9")
+val numberDigits = textDigits.mapKeys { it.value }
+val digits = textDigits.entries + numberDigits.entries
+val maxTextDigitLength = textDigits.keys.maxOf { it.length }
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+
+    fun part1(input: List<String>) =
+            input.sumOf { char -> char.filter { it.isDigit() }.map { it.toString() }.let { it.first() + it.last() }.toInt() }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        fun String.firstDigit() = digits.filter { it.key in this }.minByOrNull { indexOf(it.key) }?.value
+        fun String.valueOf() = windowed(maxTextDigitLength, partialWindows = true)
+                .mapNotNull(String::firstDigit)
+                .let { it.first() + it.last() }
+                .toInt()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        return input.sumOf(String::valueOf)
+    }
 
     val input = readInput("Day01")
     part1(input).println()
