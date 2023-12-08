dcc98a3c762312d9a71982f2bd1c205e642f2b81
eric <eric.martori.l@gmail.com>
2023-12-08 16:48:11 +0100

    solve day 8

---
+++ src/Day08.kt
@@ -0,0 +1,48 @@
+private fun String.asPair() = removeSurrounding("(", ")").split(", ").let { (f, s) -> f to s }
+private fun String.endLessRepeat() = sequence { while (true) this.yieldAll(toList()) }
+private fun List<String>.parseMap() = associate { line -> line.split(" = ").let { (k, v) -> k to v.asPair() } }
+
+typealias Problem = Pair<Sequence<Char>, Map<String, Pair<String, String>>>
+
+val Problem.instructions get() = first
+val Problem.map get() = second
+
+fun main() {
+
+    fun List<String>.parseProblem() = let {
+        it.first() to it.drop(2)
+    }.let { (instructions, map) ->
+        instructions.endLessRepeat() to map.parseMap()
+    }
+
+    fun Problem.solve(startPoint: String, isEndNode: (String) -> Boolean) =
+        instructions.scan(startPoint) { acc, direction ->
+            when (direction) {
+                'L' -> map[acc]!!.first
+                'R' -> map[acc]!!.second
+                else -> error("wrong direction")
+            }
+        }.takeWhile { !isEndNode(it) }.count()
+
+    fun part1(input: List<String>) = input.parseProblem().solve("AAA") { it == "ZZZ" }
+
+    fun part2(input: List<String>) = input.parseProblem().let { problem ->
+        problem.map.keys.filter { it.endsWith("A") }.map { startPoint ->
+            problem.solve(startPoint) { it.endsWith("Z") }
+        }
+    }.map(Int::toLong).lcm()
+
+    val testInput = readInput("Day08Test")
+    check(part1(testInput) == 2)
+
+    val testInput2 = readInput("Day08Test2")
+    check(part1(testInput2) == 6)
+
+    val testInput3 = readInput("Day08Test3")
+    check(part2(testInput3) == 6L)
+
+    val input = readInput("Day08")
+
+    part1(input).println()
+    part2(input).println()
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -21,3 +21,32 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+infix fun Long.lcm(b: Long): Long {
+    val larger = maxOf(this, b)
+    val maxLcm = this * b
+    var lcm = larger
+    while (lcm <= maxLcm) {
+        if (lcm % this == 0L && lcm % b == 0L) {
+            return lcm
+        }
+        lcm += larger
+    }
+    return maxLcm
+}
+
+infix fun Int.lcm(b: Int): Int {
+    val larger = maxOf(this, b)
+    val maxLcm = this * b
+    var lcm = larger
+    while (lcm <= maxLcm) {
+        if (lcm % this == 0 && lcm % b == 0) {
+            return lcm
+        }
+        lcm += larger
+    }
+    return maxLcm
+}
+
+fun List<Int>.lcm() = reduce(Int::lcm)
+fun List<Long>.lcm() = reduce(Long::lcm)
