b8b73d9bdffb755e1aec8c4cf86244caee35061e
eric <eric.martori.l@gmail.com>
2023-12-10 19:18:12 +0100

    WIP day 10, there is an error in part 2

--- src/Day10.kt
+++ src/Day10.kt
@@ -3,10 +3,10 @@ enum class TileType(val char: Char, val connections: Set<Direction>) {
     EW('-', setOf(Direction.E, Direction.W)),
     NE('L', setOf(Direction.N, Direction.E)),
     NW('J', setOf(Direction.N, Direction.W)),
-    WS('7', setOf(Direction.W, Direction.S)),
-    ES('F', setOf(Direction.E, Direction.S)),
+    SW('7', setOf(Direction.W, Direction.S)),
+    SE('F', setOf(Direction.E, Direction.S)),
     G('.', emptySet()),
-    S('S', Direction.entries.toSet());
+    Start('S', Direction.entries.toSet());
 
     companion object {
         val map = entries.associateBy { it.char }
@@ -17,77 +17,115 @@ enum class TileType(val char: Char, val connections: Set<Direction>) {
 }
 
 enum class Direction {
-    N {
-        override fun reverse() = S
-    },
-    S {
-        override fun reverse() = N
-    },
-    E {
-        override fun reverse() = W
-    },
-    W {
-        override fun reverse() = E
-    };
-
-    abstract fun reverse(): Direction
+    N, S, E, W
 }
 
-data class Position(val x: Int, val y: Int)
+val Direction.reverse
+    get() = when (this) {
+        Direction.N -> Direction.S
+        Direction.S -> Direction.N
+        Direction.E -> Direction.W
+        Direction.W -> Direction.E
+    }
+
+enum class Side {
+    Outside, Inside, Loop
+}
+
+fun Side.otherSide() = when (this) {
+    Side.Outside -> Side.Inside
+    Side.Inside -> Side.Outside
+    Side.Loop -> Side.Loop
+}
+
+data class Position(val x: Int, val y: Int) {
+    fun move(direction: Direction) = when (direction) {
+        Direction.N -> copy(y = y - 1)
+        Direction.S -> copy(y = y + 1)
+        Direction.E -> copy(x = x + 1)
+        Direction.W -> copy(x = x - 1)
+    }
+}
 
 typealias TileMatrix = List<List<Tile>>
 
 operator fun TileMatrix.get(position: Position) = runCatching { this[position.y][position.x] }.getOrNull()
 
-data class Tile(val type: TileType, val position: Position)
+data class Tile(val type: TileType, val position: Position, var side: Side? = null)
 
 data class TileMap(
     val start: Tile, val matrix: TileMatrix
 ) {
-    constructor(tileMatrix: TileMatrix) : this(tileMatrix.flatten().first { it.type == TileType.S }, tileMatrix)
 
-    private fun Tile.move(direction: Direction) = when (direction) {
-        Direction.N -> position.copy(y = position.y - 1)
-        Direction.S -> position.copy(y = position.y + 1)
-        Direction.E -> position.copy(x = position.x + 1)
-        Direction.W -> position.copy(x = position.x - 1)
-    }.let { matrix[it] }
+    constructor(tileMatrix: TileMatrix) : this(tileMatrix.flatten().first { it.type == TileType.Start }, tileMatrix)
 
-    private fun Tile.neighbours() =
+    private fun Tile.move(direction: Direction) =
+        matrix[position.move(direction)]
+
+    private fun Tile.getConnectingNeighbours() =
         type.connections.mapNotNull { direction ->
-            move(direction)?.takeIf { direction.reverse() in it.type.connections }
+            move(direction)?.takeIf { direction.reverse in it.type.connections }
         }
 
-    private fun Tile.nextFrom(prev: Tile) = neighbours().first { it != prev }
+    private fun Tile.nextFrom(prev: Tile) = getConnectingNeighbours().first { it != prev }
 
-    fun buildLoop() =
-        listOf(start) + generateSequence(start to start.neighbours().first()) { (prev, current) ->
+    val mainLoop by lazy {
+        listOf(start) + generateSequence(start to start.getConnectingNeighbours().first()) { (prev, current) ->
             current to current.nextFrom(prev)
         }.map { it.second }.takeWhile { it != start }.toList()
+    }
+
+    val insideTiles by lazy {
+        matrix.flatten().filter { it.side == Side.Inside }
+    }
+
+    init {
+        markSides()
+    }
+
+    private fun markSides() {
+        mainLoop.forEach { it.side = Side.Loop }
+        matrix.forEach { line ->
+            var side = Side.Outside
+            var previous: Direction? = null
+            line.forEach { tile ->
+                if (tile.side == null) tile.side = side
+                else if (tile.type == TileType.NS) side = side.otherSide()
+                else if (previous == null) {
+                    if (tile.type == TileType.NE || tile.type == TileType.NW) previous = Direction.N
+                    else if (tile.type == TileType.SE || tile.type == TileType.SW) previous = Direction.S
+                } else {
+                    if (previous == Direction.N) {
+                        if (tile.type == TileType.NE || tile.type == TileType.NW) previous = null
+                        else if (tile.type == TileType.SE || tile.type == TileType.SW) {
+                            side = side.otherSide()
+                            previous = null
+                        }
+                    } else {
+                        if (tile.type == TileType.SE || tile.type == TileType.SW) previous = null
+                        else if (tile.type == TileType.NE || tile.type == TileType.NW) {
+                            side = side.otherSide()
+                            previous = null
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
 
 fun main() {
     fun List<String>.toTileMap() = mapIndexed { y, line ->
-        line.mapIndexed() { x, char -> Tile(TileType[char], Position(x, y)) }
+        line.mapIndexed { x, char -> Tile(TileType[char], Position(x, y)) }
     }.let { TileMap(it) }
 
-    fun part1(input: List<String>) = input.toTileMap().buildLoop().size / 2
-    fun part2(input: List<String>) = 5
-
-    val testInput = readInput("Day10Test")
-
-    val testInput2 = readInput("Day10Test2")
-
-    testInput.toTileMap().buildLoop()
-
-    check(part1(testInput) == 4)
-    check(part1(testInput2) == 8)
-//    check(part2(testInput) == 5)
+    fun part1(input: List<String>) = input.toTileMap().mainLoop.size / 2
+    fun part2(input: List<String>) = input.toTileMap().insideTiles.count()
 
     val input = readInput("Day10")
     part1(input).println()
-    check(part1(input) == 6864)
-//    part2(input).println()
+    part2(input).println()
 }
 
 
