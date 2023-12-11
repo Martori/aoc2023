8299f83bd86d83d07140ae4415dc6c246b8c2677
Eric Martori <eric.martori@nestle.com>
2023-12-11 11:43:22 +0100

    refactor and add tests

--- src/Day11.kt
+++ src/Day11.kt
@@ -10,33 +10,35 @@ private fun List<String>.getGalaxiesPositions() = flatMapIndexed { row, line ->
     }.filterNotNull()
 }
 
-private fun Galaxies.expandSpace(factor: Long): Galaxies {
-    val (x, y) = unzip()
-    return x.expand(factor).zip(y.expand(factor))
+private fun Galaxies.expandSpace(factor: Long) = unzip().let { (xs, ys) ->
+    xs.expand(factor).zip(ys.expand(factor))
 }
 
-const val expansionFactor = 1000000L
+private fun List<Long>.expand(factor: Long) =
+    map { position -> position + (factor - 1) * (position - distinct().count { it < position }) }
 
-private fun List<Long>.expand(factor: Long) = map { position -> position + (factor-1)*(position - distinct().count { it < position }) }
 private infix fun Galaxy.distanceTo(other: Galaxy) =
     maxOf(x, other.x) - minOf(x, other.x) + maxOf(y, other.y) - minOf(y, other.y)
 
-private fun Galaxy.distancesTo(others: Galaxies) =
+private fun Galaxy.distanceTo(others: Galaxies) =
     others.map { this distanceTo it }
 
-fun Galaxies.sumAllDistances(): Long {
-    if (size <= 1) return 0
-    val first = first()
-    val others = drop(1)
-    return first.distancesTo(others).sum() + others.sumAllDistances()
-}
+private fun Galaxies.sumAllDistances(): Long =
+    if (size <= 1) 0
+    else drop(1).let { others ->
+        first().distanceTo(others).sum() + others.sumAllDistances()
+    }
 
 fun main() {
-    fun part1(input: List<String>) = input.getGalaxiesPositions().expandSpace(2).sumAllDistances()
-    fun part2(input: List<String>) = input.getGalaxiesPositions().expandSpace(expansionFactor).sumAllDistances()
+    fun List<String>.solve(factor: Long) = getGalaxiesPositions().expandSpace(factor).sumAllDistances()
+
+    fun part1(input: List<String>) = input.solve(2)
+    fun part2(input: List<String>) = input.solve(1000000)
 
     val testInput = readInput("Day11Test")
     check(part1(testInput) == 374L)
+    check(testInput.solve(10) == 1030L)
+    check(testInput.solve(100) == 8410L)
 
     val input = readInput("Day11")
     part1(input).println()
